af38f506008b21d7ce769f4bcd87e7d4be3fe8a0
lietk12 <lietk12@gmail.com>
2010-12-13 15:57:45 -0500

    When assembler() is not given replacement arrays, skip replacement array stuff; handle urlencode() properly now.

--- assembler.php
+++ assembler.php
@@ -7,19 +7,26 @@ function assemble( $profileIDs, $subject, $message, $replacements )
 		$outputs[$i] = "http://www.erepublik.com/en/messages/compose/" . $profileIDs[$i] . "?message_subject=" . $subject . "&message_body=" . $message;
 	}
 	
-	// Walk through the array of replacement arrays
-	for ($r = 0; $r < count($replacements); $r++) {
-		// Assume that, if a replacement array is shorter than the profile ID array, all further replacements will be blank
-		while (count($outputs) > count($replacements[$r])) {
-			$replacements[$r][] = "";
-		}
-		// With the current replacement array, replace the field tag of the current replacement array with the corresponding string for each element of the output array
-		for ($i = 0; $i < count($outputs); $i++) {
-			$outputs[$i] = str_replace( "{{FIELD" . ($r + 1) . "}}", $replacements[$r][$i], $outputs[$i] );
+	if (isset( $replacements )) {
+		// Walk through the array of replacement arrays
+		for ($r = 0; $r < count($replacements); $r++) {
+			// Assume that, if a replacement array is shorter than the profile ID array, all further replacements will be blank
+			while (count($outputs) > count($replacements[$r])) {
+				$replacements[$r][] = "";
+			}
+			// With the current replacement array, replace the field tag of the current replacement array with the corresponding string for each element of the output array
+			for ($i = 0; $i < count($outputs); $i++) {
+				$outputs[$i] = str_replace( "{{FIELD" . ($r + 1) . "}}", $replacements[$r][$i], $outputs[$i] );
+			}
 		}
 	}
+	
 	// Encode each URL to make it valid
-	return urlencode($outputs);
+	for ($i = 0; $i < count($outputs); $i++) {
+		$outputs[$i] = urlencode( $outputs[$i] );
+	}
+	
+	return $outputs;
 }
 
 ?>
