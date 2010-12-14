5beffc7bd411563e88187da1fd21658815c4bfdc
lietk12 <lietk12@gmail.com>
2010-12-13 20:56:44 -0500

    Removed outputFormatters.php's test code; added index.php as simple web frontend

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Mass-Mailer</title>
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+<!--<h2>The mass mailer is currently unusable</h2>-->
+
+<?php
+include 'assembler.php';
+include 'outputFormatters.php';
+
+	$recipients = stripslashes( $_POST["recipients"] );
+	$subject = stripslashes( $_POST["subject"] );
+	$message = stripslashes( $_POST["message"] );
+	
+	if (isset($_POST['submit'])) {
+		if ((strlen( $recipients ) == 0) || (strlen( $message ) == 0) || (strlen( $subject ) == 0)) {
+			$output = "Your recipient list, subject, AND message ALL must have things in them!";
+		} else {
+			$recipientIDs = idToIdNum( urlToId( explode( "\n", $recipients ) ) );
+			$output = generateHtmlHrefs( assemble( $recipientIDs, $subject, $message ), $recipientIDs, true );
+		}
+		// Set up a table for prettier layout.
+		echo "<table cellpadding=\"5\">";
+			echo "<tr align=\"left\">";
+				echo "<th>Links</th>";
+				echo "<th>Message Data</th>";
+			echo "</tr>";
+			echo "<tr valign=\"top\">";
+				echo "<td><p>" . $output . "</p></td>";
+				echo "<td>";
+	}
+?>
+<p>This mass mailer is a simpler version of <a href="http://erep.thepenry.net/mailer.php">AndraX2000's mass mailer</a>.  For the recipients list, paste in URLs (e.g. 
+http://www.erepublik.com/en/citizen/profile/2 ), one URL per line.  You can also use profile IDs in the form of a "#" with the number afterwards (e.g. #2).</p>
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<table>
+<tr valign="top">
+<td>
+Recipients: <br />
+<textarea name="recipients" rows="10" cols="52" wrap="off" >
+<?php echo $recipients; ?></textarea><br /><br />
+Subject:<br />
+<input type="text" name="subject" value="<?php echo $subject; ?>" size="52" maxlength="50" /> <br /><br />
+Message:<br />
+<textarea name="message" rows="10" cols="52" wrap="soft">
+<?php echo $message; ?></textarea><br />
+<input type="submit" value="submit" name="submit" />
+</td>
+</tr>
+</table>
+</form>
+
+<p>Bug reports go to lietk12.</p>
+
+<?php
+	if (isset($_POST['submit'])) {
+		// Finish the table
+				echo "</td>";
+			echo "</tr>";
+		echo "</table>";
+	}
+?>
+
+</body>
+
+</html>
--- outputFormatters.php
+++ outputFormatters.php
@@ -1,11 +1,5 @@
 <?php
 
-$messageUrls = array( "http://www.erepublik.com/en/messages/compose/2499294?message_subject=Hello%2C+Avruch&message_body=This+is+a+super-secret+message+to+buy+some+food+for+me.++You+are+person+%231+in+my+PM+list.+I+think+you+are+a+cool+guy.", "http://www.erepublik.com/en/messages/compose/1492450?message_subject=Hello%2C+merschel&message_body=This+is+a+special+message+to+check+the+forums.++You+are+person+%232+in+my+PM+list.+Nice+to+meet+you." );
-
-$profileIDs = array( 2499294, 1492450 );
-
-print_r( generateHtmlHrefs( $messageUrls, $profileIDs, false ) );
-
 function generateHtmlHrefs( $messageUrls, $profileIDs, $whetherToOpenInNewWindow ) {
 	for ($i = 0; $i < count( $messageUrls ); $i++) {
 		if ( $whetherToOpenInNewWindow ) {
