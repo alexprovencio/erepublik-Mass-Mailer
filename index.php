be30747467b31b7a8b6f8766fb24610aa14f77e7
Ethan <lietk12@gmail.com>
2010-12-14 18:33:13 -0500

    Switched to divs for layout.  Prettified index.php

--- index.php
+++ index.php
@@ -13,8 +13,6 @@
 </head>
 
 <body>
-<h1>lietk12's eRepublik Mass Mailer</h1>
-<!--<h2>The mass mailer is currently unusable</h2>-->
 
 <?php
 include 'assembler.php';
@@ -23,7 +21,7 @@ include 'inputInterface.php';
 	
 	$submit = $_GET['submit'];
 	$inputType = $_GET['input'];
-	if ($submit == "submit" || $inputType == "get") {
+	if ($submit == "Generate" || $inputType == "get") {
 		$inputType = "get";
 		$submit = $_GET['submit'];
 	} else {
@@ -45,13 +43,21 @@ include 'inputInterface.php';
 		} else {
 			$message = stripslashes( $_POST["message"] );
 		}
-	
+?>
+
+<h1>lietk12's eRepublik Mass Mailer</h1>
+
+<?php
 	if ($inputType == "post") {
-		echo 'This mass mailer is a simpler version of <a href="http://erep.thepenry.net/mailer.php">AndraX2000\'s mass mailer</a>; for a version which allows you to bookmark specific recipients/subjects/messages, click <a href="./?input=get&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">here</a>.</p>';
+		echo '<p>This mass mailer is a simpler version of <a href="http://erep.thepenry.net/mailer.php">AndraX2000\'s mass mailer</a>; for a version which allows you to bookmark specific recipients/subjects/messages, click <a href="./?input=get&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">here</a>.</p>';
 	} else {
 		echo '<h2>(Bookmarkable Version)</h2><p>This mass mailer is a bookmarkable version of <a href="./index.php">lietk12\'s mass mailer</a>; for super-long lists of people or long messages, this version may behave strangely, in which case you should use <a href="./?input=post&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">the other version</a> instead.</p>';
 	}
-	
+?>
+
+<div id="container">
+<div id="containerrow">
+<?php
 	if (isset($submit)) {
 		if ((strlen( $recipients ) == 0) || (strlen( $message ) == 0) || (strlen( $subject ) == 0)) {
 			$output = "Your recipient list, subject, AND message ALL must have things in them!";
@@ -62,49 +68,54 @@ include 'inputInterface.php';
 				$output = $output . $outputArray[$i] . '<br />';
 			}
 		}
-		// Set up a table for prettier layout.
-		echo "<table cellpadding=\"5\">";
-			echo "<tr align=\"left\">";
-				echo "<th>Links</th>";
-				echo "<th>Message Data</th>";
-			echo "</tr>";
-			echo "<tr valign=\"top\">";
-				echo "<td>" . $output . "</td>";
-				echo "<td>";
+		
+		echo '<div id="left">';
+		echo '<h2>Links</h2>';
+		echo $output;
+		echo '</div>';
 	}
 ?>
-For the recipients list, as long as you have one player per line, you can use any combination of the following formats to specify the players:
-<ul>
-<li>Player profile URLs (e.g. <code>http://www.erepublik.com/en/citizen/profile/2</code> )</li>
-<li>Player PM URLs (e.g. <code>http://www.erepublik.com/en/messages/compose/2</code> )</li>
-<li>Profile IDs in the form of a "#" with the number afterwards (e.g. <code>#2</code> ).</li>
-</ul>
+
 <form method="<?php echo $inputType;?>" action="./">
-<table>
-<tr valign="top">
-<td>
+
+<div id="middle">
+<h2>Message Data</h2>
 <h3>Recipients:</h3>
 <textarea name="recipients" id="recipients" class="resizable" rows="10" cols="52" wrap="off" >
-<?php echo $recipients; ?></textarea><br /><br />
+<?php echo $recipients; ?></textarea>
+
 <h3>Subject:</h3>
-<input type="text" name="subject" id="subject" value="<?php echo $subject; ?>" size="52" maxlength="50" /> <br /><br />
+<input type="text" name="subject" id="subject" value="<?php echo $subject; ?>" size="54" maxlength="50" />
+
 <h3>Message:</h3>
 <textarea name="message" id="message" rows="10" cols="52" wrap="soft">
-<?php echo $message; ?></textarea><br />
-<input type="submit" value="submit" name="submit" />
-</td>
-</tr>
-</table>
+<?php echo $message; ?></textarea>
+
+<input type="submit" id = "submit" value="Generate" name="submit" />
+</div>
+
+<div id="right">
+<div id="help">
+	<h2>Notes</h2>
+	For the recipients list, as long as you have one player per line, you can use any combination of the following formats to specify the players:
+	<ul>
+		<li>Player profile URLs (e.g. <code>http://www.erepublik.com/en/citizen/profile/2</code> )</li>
+		<li>Player PM URLs (e.g. <code>http://www.erepublik.com/en/messages/compose/2</code> )</li>
+		<li>Profile IDs in the form of a "#" with the number afterwards (e.g. <code>#2</code> ).</li>
+	</ul>
+</div>
+<!--
+<div id="replacements">
+<h2>Replacement Data</h2>
+Blah.
+</div>-->
+</div>
+
 </form>
 
-<?php
-	if (isset($_POST['submit'])) {
-		// Finish the table
-				echo "</td>";
-			echo "</tr>";
-		echo "</table>";
-	}
-?>
+</div>
+</div>
+
 <p id="footer">Bugs?  Feature requests?  Design suggestions?  Comments?  Please talk to <a href="http://www.erepublik.com/en/citizen/profile/1242030">lietk12</a>!<br />
 This project is being developed at <a
 href="https://github.com/lietk12/erepublik-Mass-Mailer/tree/shellscripted">github</a>&#151;contact lietk12 if you want to contribute.</p>
