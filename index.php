0a8323a25e0e71215ed1f98ae0713c5c973f8138
Ethan <lietk12@gmail.com>
2010-12-15 20:08:43 -0500

    Started work on making a selector for how data will be output

--- index.php
+++ index.php
@@ -51,21 +51,12 @@ include 'inputInterface.php';
 	} else {
 		$targetmode = stripslashes( $_GET["target"] );
 	}
-?>
-
-<h1>lietk12's eRepublik Mass Mailer (v0.5)</h1>
-
-<?php
-	if ($inputType == "post") {
-		echo '<p>This mass mailer is a simpler version of <a href="http://erep.thepenry.net/mailer.php">AndraX2000\'s mass mailer</a>; for a version which allows you to bookmark specific recipients/subjects/messages, click <a href="./?input=get&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">here</a>.</p>';
+	if (isset($_POST["output"])) {
+		$outputmode = stripslashes( $_POST["output"] );
 	} else {
-		echo '<h2>(Bookmarkable Version)</h2><p>This mass mailer is a bookmarkable version of <a href="./index.php">lietk12\'s mass mailer</a>; for super-long lists of people or long messages, this version may behave strangely, in which case you should use <a href="./?input=post&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">the other version</a> instead.</p>';
+		$outputmode = stripslashes( $_GET["output"] );
 	}
-?>
-
-<div id="container">
-<div id="containerrow">
-<?php
+	
 	if (isset($submit)) {
 		if ((strlen( $recipients ) == 0) || (strlen( $message ) == 0) || (strlen( $subject ) == 0)) {
 			$output = "Your recipient list, subject, AND message ALL must have things in them!";
@@ -86,16 +77,34 @@ include 'inputInterface.php';
 			$recipientIDs = idToIdNum( urlToId( explode( "\n", $recipients ) ) );
 			// Generate link array
 			$outputArray = generateHtmlHrefs( assemble( $recipientIDs, $subject, $message, $replacements ), $recipientIDs, $target );
-			// Print array
+			// Split array for one element per html line
 			for ($i = 0; $i < count( $outputArray ); $i++) {
 				$output = $output . $outputArray[$i] . '<br />';
 			}
 		}
-		
-		echo '<div id="left">';
-		echo '<h2>Links</h2>';
-		echo $output;
-		echo '</div>';
+		if ($outputmode == "self") {
+		}
+?>
+
+<h1>lietk12's eRepublik Mass Mailer (v0.5)</h1>
+
+<?php
+	if ($inputType == "post") {
+		echo '<p>This mass mailer is a simpler version of <a href="http://erep.thepenry.net/mailer.php">AndraX2000\'s mass mailer</a>; for a version which allows you to bookmark specific recipients/subjects/messages, click <a href="./?input=get&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">here</a>.</p>';
+	} else {
+		echo '<h2>(Bookmarkable Version)</h2><p>This mass mailer is a bookmarkable version of <a href="./index.php">lietk12\'s mass mailer</a>; for super-long lists of people or long messages, this version may behave strangely, in which case you should use <a href="./?input=post&recipients=' . urlencode( $recipients ) . '&subject=' . urlencode( $subject ) . '&message=' . urlencode( $message ) . '">the other version</a> instead.</p>';
+	}
+?>
+
+<div id="container">
+<div id="containerrow">
+<?php		
+		if ($outputmode != "self" && $outputmode != "new") {
+			echo '<div id="left">';
+			echo '<h2>Links</h2>';
+			echo $output;
+			echo '</div>';
+		}
 	}
 ?>
 
@@ -114,6 +123,13 @@ include 'inputInterface.php';
 <textarea name="message" id="message" rows="10" cols="52" wrap="soft">
 <?php echo $message; ?></textarea>
 <h2>Options:</h2>
+The list of links should go
+<select name="output">
+	<option value="column" <?php if ($outputmode == "column") {echo 'selected="selected"';}?>>in a column to the left</option>
+	<option value="new" <?php if ($outputmode == "new") {echo 'selected="selected"';}?>>in a new tab</option>
+	<option value="self" <?php if ($outputmode == "self") {echo 'selected="selected"';}?>>in this tab as a new page</option>
+</select> (Note: this feature is currently nonfunctional)
+<br />
 Links, when left-clicked, will 
 <select name="target">
 	<option value="self" <?php if ($targetmode == "self") {echo 'selected="selected"';}?>>open in this tab</option>
